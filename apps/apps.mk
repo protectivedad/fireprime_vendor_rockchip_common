ae682f83883fbae4dc7f0047509259fb17c26d21
Anthony Sepa <anthonysepa@yahoo.ca>
2017-04-05 21:39:14 -0300

    Initial sync with fireprime

---
+++ apps/apps.mk
@@ -0,0 +1,77 @@
+
+PRODUCT_PACKAGES += \
+    RkApkinstaller  \
+    RkExpe          \
+    RKUpdateService \
+	userExperienceService
+
+ifneq ($(strip $(TARGET_BOARD_PLATFORM)), sofia3gr)
+PRODUCT_PACKAGES += \
+    MediaFloat      \
+    GoogleEmail
+
+endif
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk3288)
+PRODUCT_PACKAGES += \
+    WinStartService
+endif
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk312x)
+PRODUCT_PACKAGES += \
+    RkVideoPlayer
+else
+ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
+PRODUCT_PACKAGES += \
+    RkBoxVideoPlayer
+else
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)), sofia3gr)
+PRODUCT_PACKAGES += \
+    Rk3grVideoPlayer
+else
+PRODUCT_PACKAGES += \
+    Rk4kVideoPlayer
+endif
+endif
+endif
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)), sofia3gr)
+PRODUCT_PACKAGES += \
+    Rk3grExplorer
+else
+PRODUCT_PACKAGES += \
+    RkExplorer
+endif
+
+ifeq ($(strip $(BOARD_HAS_STRESSTEST_APP)), true)
+    PRODUCT_PACKAGES += \
+    StressTest \
+    DeviceTest
+endif
+
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)), sofia3gr)
+PRODUCT_PACKAGES += \
+    ituxd\
+    com.rockchip.alarmhelper \
+    RFTest
+
+#PRODUCT_COPY_FILES += \
+#        $(LOCAL_PATH)/ituxd/lib/x86/libthermalJNI.so:system/lib/libthermalJNI.so
+endif
+
+
+###########for box app ################
+ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
+PRODUCT_PACKAGES += \
+    AllApp	\
+    eHomeMediaCenter_box	\
+    ITVLauncher	\
+    RKGameControlSettingV1.0.1	\
+    RkMusic	\
+    Settings2
+  ifeq ($(strip $(BOARD_USE_LOW_MEM256)), true)
+        PRODUCT_PACKAGES += \
+              SimpleLauncher
+  endif
+endif
